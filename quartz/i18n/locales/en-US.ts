8f25f1947466329f4d978151f3bed36950f97bb3
r-ys <elysserinoab14@gmail.com>
2026-06-12 17:33:58 +0800

    Quartz sync: Jun 12, 2026, 5:33 PM

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -66,7 +66,7 @@ export default {
       lastFewNotes: ({ count }) => `Last ${count} notes`,
     },
     error: {
-      title: "Not Found",
+      title: "uh oh !",
       notFound: "Either this page is private or doesn't exist.",
       home: "Return to Homepage",
     },
